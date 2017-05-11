7575640d2322dd62beab40c96550ec2b43b98165
Anastasiia <igoninan87@gmail.com>
2017-05-11 12:32:58 +0300

    Add Accordion and Carousel

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'normalize.css';
+import './style.scss';
+
+import './img/photo.svg';
+import './img/creativity.svg';
+import './img/design.svg';
+
+import './img/joshua.png';
+import './img/mike.png';
+import './img/lola.png';
---
+++ webpack/url.js
@@ -0,0 +1,34 @@
+module.exports = function (path) {
+  return {
+    module: {
+      rules: [{
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 1,
+            name: `${path}/[name].[ext]?[hash]`,
+          },
+        }, {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              quality: 65,
+              progressive: true,
+            },
+            pngquant: {
+              quality: '50-65',
+              speed: 4,
+            },
+            svgo: {
+              plugins: [
+                { removeViewBox: false },
+                { removeEmptyAttrs: false },
+              ],
+            },
+          },
+        }],
+      }],
+    },
+  };
+};
